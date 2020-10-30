364162c4d9d59e7a3bb37aa8443d1f03f04af891
JR Mishra <jrmnitdgp@gmail.com>
2020-10-30 19:47:06 +0530

    [JR Mishra]Add. Test project initialized

---
+++ TestCabInvoiceGenerator/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace TestCabInvoiceGenerator
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
